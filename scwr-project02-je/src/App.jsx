6197d9aa1ba658e20cb0b7817c2127eeaf60e256
jegyes <jegyes@madisoncollege.edu>
2025-11-14 19:43:49 -0600

    app set up

--- scwr-project02-je/src/App.jsx
+++ scwr-project02-je/src/App.jsx
@@ -3,31 +3,30 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+const semiAnnuals = [
+  {
+    year: '2000',
+    yearFirstHalf: 326447,
+    yearSecondHalf:  562342,
+  },
+  {
+    year: '2008',
+    yearFirstHalf: 424587,
+    yearSecondHalf:  764254,
+  },
+  {
+    year: '2012',
+    yearFirstHalf: 756230,
+    yearSecondHalf: 695323,
+  }
+]
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Report of Annual Totals for Select Years</h1>
     </>
   )
 }
