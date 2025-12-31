52f145cbffa989569e919bd498a5c2a397a6f891
jegyes <jegyes@madisoncollege.edu>
2025-12-31 14:56:05 -0600

    updateInputValues function - enabled values - inputting

--- cryptoapp/src/App.jsx
+++ cryptoapp/src/App.jsx
@@ -9,15 +9,22 @@ import './App.css';
 function App() {
   // Create coins variable and set to empty array
   const [coins, updateCoins] = useState([])
+  // Create additional state to hold user input for limit and start properties
+  const [input, updateInput] = useState({ limit: 5, start: 0 })
+
+  // Create a new function to allow users to update the input values
+  function updateInputValues(type, value) {
+    updateInput({ ...input, [type]: value })
+  }
 
   // Define function to call API (goes out to our gateway that 
   // then goes out to our lambda function and returns a js object (in app.js))
   async function fetchCoins() {
+    const { limit, start } = input
     const request = get({
       apiName: 'cryptoapi',
-      path: '/coins'
-    })
-  
+      path: `/coins?limit=${limit}&start=${start}`
+    })  
   // makes the request to the API
     const response = await request.response;
     const data = await response.body.json();
@@ -32,6 +39,17 @@ function App() {
 
   return (
     <div className="App">
+      <input
+        onChange={e => updateInputValues('limit', e.target.value)}
+        placeholder="limit"
+      />
+      <input
+        placeholder="start"
+        onChange={e => updateInputValues('start', e.target.value)}
+      />
+
+      {/* Add button to the UI to give user the option to call the API */}
+      <button onClick={fetchCoins}>Fetch Coins</button>
       {
         coins.map((coin, index) => (
           <div key={index}>
