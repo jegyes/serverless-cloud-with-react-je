07e0aa536f845b058048d62922fac39c2f831d0a
jegyes <jegyes@madisoncollege.edu>
2026-01-12 22:44:38 -0600

    getting_started next.js tutorial - styles
    getting started with next.js tutorial - through chapter 3 - optimizing fonts and images

---
+++ next_tutorials/nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,16 @@
+// this file acts as the main template for our app in effect
+
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
